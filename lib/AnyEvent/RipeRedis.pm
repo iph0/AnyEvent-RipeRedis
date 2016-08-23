57c6aa037ffea1c039ba1ad3b3dcb5d868e54172
Eugene Ponizovsky <ponizovsky@gmail.com>
2016-08-23 18:44:15 +0300

    Version increased

--- lib/AnyEvent/RipeRedis.pm
+++ lib/AnyEvent/RipeRedis.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use base qw( Exporter );
 
-our $VERSION = '0.07_02';
+our $VERSION = '0.07_03';
 
 use AnyEvent::RipeRedis::Error;
 
--- lib/AnyEvent/RipeRedis/Error.pm
+++ lib/AnyEvent/RipeRedis/Error.pm
@@ -4,7 +4,7 @@ use 5.008000;
 use strict;
 use warnings;
 
-our $VERSION = '0.07_02';
+our $VERSION = '0.07_03';
 
 
 sub new {
